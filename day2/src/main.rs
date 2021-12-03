c1d263fd4247b3f730253292d6ad17749be2363b
Sara Beaudet <sara@saratonin.dev>
2021-12-03 12:04:28 -0500

    d2p2 solution

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
     let my_inp = read_lines();
 
-	let part_1_sub: Submarine = part_1(my_inp);
+	let part_1_sub: Submarine = part_1(&my_inp);
+	let part_2_sub: Submarine = part_2(&my_inp);
 
 	println!("Part 1 deets: {}", part_1_sub);
 	println!("Part 1 answer: {}", part_1_sub.hor_poz * part_1_sub.depth);
+
+	println!("Part 2 deets: {}", part_2_sub);
+	println!("Part 2 answer: {}", part_2_sub.hor_poz * part_2_sub.depth);
 }
 
 fn read_lines() -> Vec<Instruction> {
@@ -20,6 +24,7 @@ fn parse_instruction(instruction: &str) -> Instruction{
 			&"forward" => return Instruction::Forward(amt.parse::<u32>().unwrap()),
 			&"down" => return Instruction::Down(amt.parse::<u32>().unwrap()),
 			&"up" => return Instruction::Up(amt.parse::<u32>().unwrap()),
+			&"aim" => return Instruction::Aim(amt.parse::<u32>().unwrap()),
 			_ => panic!("invalid instruction"),
 		}
 	} else {
@@ -30,11 +35,16 @@ fn parse_instruction(instruction: &str) -> Instruction{
 struct Submarine {
 	hor_poz: u32,
 	depth: u32,
+	aim: Option<u32>,
 }
 
 impl std::fmt::Display for Submarine {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "(Horizontal position: {}, Depth: {})", self.hor_poz, self.depth)
+        if let Some(_aim) = self.aim {
+			write!(f, "(Horizontal position: {}, Depth: {}, Aim: {})", self.hor_poz, self.depth, _aim)
+		} else {
+			write!(f, "(Horizontal position: {}, Depth: {})", self.hor_poz, self.depth)
+		}
     }
 }
 
@@ -42,13 +52,15 @@ enum Instruction {
 	Forward(u32),
 	Down(u32),
 	Up(u32),
+	Aim(u32),
 }
 
-fn part_1(input: Vec<Instruction>) -> Submarine {
+fn part_1(input: &Vec<Instruction>) -> Submarine {
 
 	let mut sub = Submarine {
 		hor_poz: 0,
 		depth: 0,
+		aim: None,
 	};
 
 	for instruction in input {
@@ -56,6 +68,32 @@ fn part_1(input: Vec<Instruction>) -> Submarine {
 			Instruction::Forward(amt) => sub.hor_poz += amt,
 			Instruction::Down(amt) => sub.depth += amt,
 			Instruction::Up(amt) => sub.depth -= amt,
+			_ => panic!("unrecognised instruction"),
+		}
+	}
+
+	return sub;
+}
+
+fn part_2(input: &Vec<Instruction>) -> Submarine {
+	
+	let mut sub = Submarine {
+		hor_poz: 0,
+		depth: 0,
+		aim: Some(0),
+	};
+
+	for instruction in input {
+		if let Some(aim) = sub.aim {
+			match instruction {
+				Instruction::Forward(amt) => {
+					sub.hor_poz += amt;
+					sub.depth += amt * aim;
+				},
+				Instruction::Down(amt) => sub.aim = Some(aim + amt),
+				Instruction::Up(amt) => sub.aim = Some(aim - amt),
+				_ => panic!("unrecognised instruction"),
+			}
 		}
 	}
 
