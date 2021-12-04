8feeb69a4e7a5b4370c638de8d9087ce532ae98d
Sara Beaudet <sara@saratonin.dev>
2021-12-04 00:14:10 -0500

    more restructuring

--- lib.rs
+++ lib.rs
@@ -1,5 +1,4 @@
 pub fn parse_input(input_path: &str) {
 	let input = std::fs::read_to_string(input_path).unwrap();
-	println!("INPUTBASKJHFDAS: {}", input);
-	// input.lines().map(|l| l.parse().unwrap()).collect()
+	input.lines().map(|l| l.parse().unwrap()).collect()
 }
