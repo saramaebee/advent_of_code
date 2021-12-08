df8219c38d5944e81ca5229a01101741286ff77e
Sara Beaudet <sara@saratonin.dev>
2021-12-08 11:29:21 -0500

    day 4 changes

--- src/aoc2021d4.rs
+++ src/aoc2021d4.rs
@@ -1,19 +1,13 @@
 pub fn p1(_input: Vec<String>) -> String {
-	let nums_called: Vec<i32> = _input[0]
+	let nums_called: Vec<isize> = _input[0]
 		.split(",")
-		.map(|l| l.parse::<i32>().unwrap())
+		.map(|l| l.parse::<isize>().unwrap())
 		.collect();
 
 	let mut playing_boards = extract_boards(_input);
 
 	for num_called in nums_called.iter().peekable() {
-		playing_boards = playing_boards.into_iter().map(|l| {
-			let k = l.clone().iter().map(|n| {
-				let o = n.clone().iter().map(|p| if *p == *num_called { -1 } else { *p }).collect();
-				o
-			}).collect();
-			k
-		}).collect();
+		playing_boards = run_number(playing_boards, *num_called);
 
 		for board in &playing_boards {
 			if check_board(board) {
@@ -30,27 +24,21 @@ pub fn p1(_input: Vec<String>) -> String {
 // but it's not working correctly on the actual text for some reason.
 
 pub fn p2(_input: Vec<String>) -> String {
-	let nums_called: Vec<i32> = _input[0]
+	let nums_called: Vec<isize> = _input[0]
 	.split(",")
-	.map(|l| l.parse::<i32>().unwrap())
+	.map(|l| l.parse::<isize>().unwrap())
 	.collect();
 
-	let mut playing_boards: Vec<Vec<Vec<i32>>> = extract_boards(_input);
+	let mut playing_boards: Vec<Vec<Vec<isize>>> = extract_boards(_input);
 
 	for num_called in nums_called.iter().peekable() {
-		playing_boards = playing_boards.into_iter().map(|l| {
-			let k = l.clone().iter().map(|n| {
-				let o = n.clone().iter().map(|p| if *p == *num_called { -1 } else { *p }).collect();
-				o
-			}).collect();
-			k
-		}).collect::<Vec<Vec<Vec<i32>>>>();
+		playing_boards = run_number(playing_boards, *num_called);
 
 		if playing_boards.len() > 1 {
 			playing_boards.retain(|l| !check_board(l));
 		} else {
 			let sum = sum_of_board(&playing_boards[0]);
-			// println!("Sum: {:?}; Last Num: {:?}; Winner: {:?}", sum, num_called, playing_boards);
+			println!("Sum: {:?}; Last Num: {:?}; Winner: {:?}", sum, num_called, playing_boards);
 			return (sum * num_called).to_string();
 		}
 	}
@@ -60,7 +48,17 @@ pub fn p2(_input: Vec<String>) -> String {
 
 // setup funcs
 
-fn check_board(_board: &Vec<Vec<i32>>) -> bool {
+fn run_number(playing_boards: Vec<Vec<Vec<isize>>>, num_called: isize) -> Vec<Vec<Vec<isize>>> {
+	playing_boards.into_iter().map(|l| {
+		let k = l.clone().iter().map(|n| {
+			let o = n.clone().iter().map(|p| if *p == num_called { -1 } else { *p }).collect();
+			o
+		}).collect();
+		k
+	}).collect::<Vec<Vec<Vec<isize>>>>()
+}
+
+fn check_board(_board: &Vec<Vec<isize>>) -> bool {
 	
 	for row in _board {
 		if row.iter().all(|l| *l == -1) {
@@ -70,7 +68,7 @@ fn check_board(_board: &Vec<Vec<i32>>) -> bool {
 	
 	for i in 0.._board.len() {
 
-		let column = &_board.clone().into_iter().map(|l| l[i]).collect::<Vec<i32>>();
+		let column = &_board.clone().into_iter().map(|l| l[i]).collect::<Vec<isize>>();
 		if column.iter().all(|l| *l == -1) {
 			return true;
 		}
@@ -79,29 +77,29 @@ fn check_board(_board: &Vec<Vec<i32>>) -> bool {
 	false
 }
 
-fn sum_of_board(vector: &Vec<Vec<i32>>)  -> i32 {
+fn sum_of_board(vector: &Vec<Vec<isize>>)  -> isize {
 
-	let board: Vec<Vec<i32>> = vector.clone().into_iter().map(|k| {
-		let m = k.into_iter().filter(|l| *l >= 0).map(|l| l).collect::<Vec<i32>>();
+	let board: Vec<Vec<isize>> = vector.clone().into_iter().map(|k| {
+		let m = k.into_iter().filter(|l| *l >= 0).map(|l| l).collect::<Vec<isize>>();
 		m.to_vec()
 	}).collect();
 
 	let mut sum = 0;
 
 	for row in board {
-		sum += row.iter().sum::<i32>();
+		sum += row.iter().sum::<isize>();
 	}
 
 	sum
 }
 
-fn extract_boards(_input: Vec<String>) -> Vec<Vec<Vec<i32>>>{
+fn extract_boards(_input: Vec<String>) -> Vec<Vec<Vec<isize>>>{
 	let input: Vec<String> = _input
 		.into_iter()
 		.filter(|l| l.len() > 0 && l.len() < 40)
 		.collect();
 
-	let boards: Vec<Vec<Vec<i32>>> = input
+	let boards: Vec<Vec<Vec<isize>>> = input
 		.chunks(5)
 		.collect::<Vec<&[String]>>()
 		.into_iter()
@@ -113,7 +111,7 @@ fn extract_boards(_input: Vec<String>) -> Vec<Vec<Vec<i32>>>{
 						.filter(|x| x.len() > 0)
 						.map(|n| {
 							if n.len() > 0 {
-								n.parse::<i32>().unwrap()
+								n.parse::<isize>().unwrap()
 							} else {
 								0
 							}
