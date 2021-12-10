841bfd971ae06975b646bc5da8775c5a8d6a6c03
Sara Beaudet <sara@saratonin.dev>
2021-12-09 23:42:39 -0500

    cleaning it up a lil

--- src/aoc2021d7.rs
+++ src/aoc2021d7.rs
@@ -1,15 +1,15 @@
 use std::ops::Range;
 
 pub fn p1(_input: Vec<String>) -> String {
-	let input: Vec<isize> = _input[0].split(",").map(|l| l.parse::<isize>().unwrap()).collect();
+	let input: Vec<usize> = _input[0].split(",").map(|l| l.parse::<usize>().unwrap()).collect();
 
-	let min: isize = *input.clone().iter().min().unwrap();
-	let max: isize = *input.clone().iter().max().unwrap();
+	let min: usize = *input.clone().iter().min().unwrap();
+	let max: usize = *input.clone().iter().max().unwrap();
 
-	let mut min_sum: isize = 9999999;
+	let mut min_sum: usize = input.clone().into_iter().map(|l| (min as isize - l as isize).abs() as usize).sum();
 
 	for i in min .. max + 1 {
-		let sum = input.clone().into_iter().map(|l| (i - l).abs()).sum();
+		let sum = input.clone().into_iter().map(|l| (i as isize - l as isize).abs() as usize).sum();
 		if sum < min_sum {
 			min_sum = sum;
 		}
@@ -19,17 +19,20 @@ pub fn p1(_input: Vec<String>) -> String {
 }
 
 pub fn p2(_input: Vec<String>) -> String {
-	let input: Vec<isize> = _input[0].split(",").map(|l| l.parse::<isize>().unwrap()).collect();
+	let input: Vec<usize> = _input[0].split(",").map(|l| l.parse::<usize>().unwrap()).collect();
 
-	let min: isize = *input.clone().iter().min().unwrap();
-	let max: isize = *input.clone().iter().max().unwrap();
+	let min: isize = *input.clone().iter().min().unwrap() as isize;
+	let max: isize = *input.clone().iter().max().unwrap() as isize;
 
-	let mut min_sum: isize = 99999999999;
+	let mut min_sum: usize = input.clone().into_iter().map(|l| {
+		sum_of_range(0 as usize .. ((min - l as isize).abs() as usize) + 1)
+	}).sum();
 
 	for i in min .. max + 1 {
 		let sum = input.clone().into_iter().map(|l| {
-			sum_of_range(0 .. (i - l).abs() + 1)
+			sum_of_range(0 as usize .. ((i - l as isize).abs() as usize) + 1)
 		}).sum();
+		min_sum = sum;
 		if sum < min_sum {
 			min_sum = sum;
 		}
@@ -39,6 +42,6 @@ pub fn p2(_input: Vec<String>) -> String {
 }
 
 // helper funcs
-fn sum_of_range( range: Range<isize>) -> isize {
+fn sum_of_range( range: Range<usize>) -> usize {
 	range.fold(0, |a, b| a + b)
 }
